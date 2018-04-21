32247d74a95d9e48c5371307bb8ff7bc813332a0
newpolaris <newpolaris@gmail.com>
2018-04-21 17:07:01 +0900

    Mie scattering

--- src/Atmosphere.cpp
+++ src/Atmosphere.cpp
@@ -26,6 +26,7 @@ glm::vec4 Atmosphere::computeIncidentLight(const glm::vec3& pos, const glm::vec3
 	const glm::vec3 SunIntensity = glm::vec3(20.f);
 	const int numSamples = 16;
 	const int numLightSamples = 8;
+    const float g = 0.76f; 
 	const float pi = glm::pi<float>();
 
 	auto t = RaySphereIntersect(pos, dir, m_Ec, m_Ar);
@@ -37,34 +38,42 @@ glm::vec4 Atmosphere::computeIncidentLight(const glm::vec3& pos, const glm::vec3
 
 	float opticalDepthR = 0.f, opticalDepthM = 0.f;
 	float ds = (tmax - tmin) / numSamples; // delta segment
-	glm::vec3 sumR(0.f);
+	glm::vec3 sumR(0.f), sumM(0.f);
 	for (int s = 0; s < numSamples; s++)
 	{
 		glm::vec3 x = pb + ds*(0.5f + s)*dir;
 		float h = glm::length(x) - m_Er;
 		float betaR = glm::exp(-h/m_Hr)*ds;
+        float betaM = glm::exp(-h/m_Hm)*ds;
 		opticalDepthR += betaR;
+        opticalDepthM += betaM;
 		auto tl = RaySphereIntersect(x, m_SunDir, m_Ec, m_Ar);
 		float lmax = tl.y, lmin = 0.f;
 		float dls = (lmax - lmin)/numLightSamples; // delta light segment
 		int l = 0;
-		float opticalDepthLightR = 0.f;
+		float opticalDepthLightR = 0.f, opticalDepthLightM = 0.f;
 		for (; l < numLightSamples; l++)
 		{
 			glm::vec3 xl = x + dls*(0.5f + l)*m_SunDir;
 			float hl = glm::length(xl) - m_Er;
 			if (hl < 0) break;
 			opticalDepthLightR += glm::exp(-hl/m_Hr)*dls;
+			opticalDepthLightM += glm::exp(-hl/m_Hm)*dls;
 		}
 		if (l < numLightSamples) continue;
-		glm::vec3 tau = m_BetaR0 * (opticalDepthR + opticalDepthLightR);
+        glm::vec3 tauR = m_BetaR0 * (opticalDepthR + opticalDepthLightR);
+        glm::vec3 tauM = 1.1f * m_BetaM0 * (opticalDepthM + opticalDepthLightM);
+		glm::vec3 tau = tauR + tauM;
 		glm::vec3 attenuation = glm::exp(-tau);
 		sumR += attenuation * betaR;
+        sumM += attenuation * betaM;
 	}
 
 	float mu = glm::dot(m_SunDir, dir);
 	float phaseR = 3.f / (16.f*pi) * (1.f + mu*mu);
-	return glm::vec4(SunIntensity * sumR * phaseR * m_BetaR0, 1.f);
+	float phaseM = 3.f / (8.f*pi) * ((1 - g*g)*(1 + mu*mu))/((2 + g*g)*pow(1 + g*g - 2*g*mu, 1.5f));
+    glm::vec3 color = sumR * phaseR * m_BetaR0 + sumM * phaseM * m_BetaM0;
+	return glm::vec4(SunIntensity * color, 1.f);
 }
 
 void Atmosphere::renderSkyDome(std::vector<glm::vec4>& image, int width, int height) const
--- src/Atmosphere.h
+++ src/Atmosphere.h
@@ -10,11 +10,13 @@ public:
 	glm::vec4 computeIncidentLight(const glm::vec3& orig, const glm::vec3& dir, float tmin, float tmax) const; 
 	void renderSkyDome(std::vector<glm::vec4>& image, int width, int height) const;
 
-	float m_Hr = 7994;
+	float m_Hr = 7994; // Rayleigh scale height
+    float m_Hm = 1200; // Mie scale height
 	float m_Ar = 6420e3; // atmosphere radius
 	float m_Er = 6360e3; // earth radius
 
 	glm::vec3 m_SunDir;
 	glm::vec3 m_Ec = glm::vec3(0.f); // earth center
 	glm::vec3 m_BetaR0 = glm::vec3(3.8e-6f, 13.5e-6f, 33.1e-6f); 
+    glm::vec3 m_BetaM0 = glm::vec3(210e-5f);
 };
